11676f82e53f5baf38a5d7f36edb0b836e5aa075
Ian Paul <iandouglaspaul@gmail.com>
2024-12-12 15:48:07 +0700

    patch: pass proper path to content.js files

--- src/popup.js
+++ src/popup.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
       chrome.scripting
         .executeScript({
           target: { tabId: tabs[0].id },
-          files: ["content.js"],
+          files: ["src/content.js"],
         })
         .then(() => {
           // Then send the message
