b3d81e86b5eb6bed9b709c54ba709fe04414f8d7
Ian Paul <iandouglaspaul@gmail.com>
2024-12-12 17:46:12 +0700

    patch: formatting for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
+  mode: "development",
+  devtool: "source-map",
   entry: {
-    background: './src/background.js',
-    content: './src/content.js',
-    popup: './src/popup.js'
+    background: "./src/background.js",
+    content: "./src/content.js",
+    popup: "./src/popup.js",
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    clean: true
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
   },
   module: {
     rules: [
@@ -20,21 +20,21 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: 'manifest.json', to: 'manifest.json' },
-        { from: 'public', to: 'public' },
-        { from: 'src/types.js', to: 'types.js' }
-      ]
-    })
-  ]
+        { from: "manifest.json", to: "manifest.json" },
+        { from: "public", to: "public" },
+        { from: "src/types.js", to: "types.js" },
+      ],
+    }),
+  ],
 };
